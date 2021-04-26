b75eeec0233ab1fc18522280028153e4622dfc77
Jack Campbell <jackcampbell19@gmail.com>
2021-04-26 16:20:48 -0700

    update

--- code/Stepper.py
+++ code/Stepper.py
@@ -10,7 +10,7 @@ GPIO.setmode(GPIO.BOARD)
 # Stepper class.
 class Stepper:
 
-    SHORTEST_DELAY = 0.002
+    SHORTEST_DELAY = 0.01 # 0.002
 
     def __init__(self, stp, dir):
         self.stp = stp
