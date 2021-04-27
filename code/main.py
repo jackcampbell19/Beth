2e6c9f7644e7d234413ee3b2171245be10e53edd
Jack Campbell <jackcampbell19@gmail.com>
2021-04-26 22:58:26 -0700

    update

--- code/main.py
+++ code/main.py
@@ -69,15 +69,15 @@ if __name__ == "__main__":
         if target_fid not in markers:
             Log.debug('Target marker not found.')
             # arm.move_to_random_position()
-            time.sleep(4)
+            time.sleep(1)
             continue
         # Calculate the vector between the current position and the target markers position
         movement_vector = markers[target_fid].center - center
         movement_vector *= -1
         if np.linalg.norm(movement_vector) < 2:
             Log.info('Target marker is within 2mm of the center of the frame.')
-            time.sleep(2)
+            time.sleep(1)
             continue
         Log.debug(movement_vector)
         arm.move_along_vector(np.array([int(movement_vector[0]), int(movement_vector[1])]))
-        time.sleep(4)
+        time.sleep(1)
