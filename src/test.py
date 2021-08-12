b45ec92aa77e33a78914b34b0e57dba394681e1d
Jack Campbell <jackcampbell19@gmail.com>
2021-08-11 17:38:01 -0700

    update

--- src/test.py
+++ src/test.py
@@ -15,15 +15,15 @@ from src.tracking.Board import Board
 # est_move =
 
 
-stockfish = Stockfish()
+# stockfish = Stockfish()
 
 
 # stockfish.set_fen_position('rnbqkbnr/ppppp1pp/8/5p2/4P3/8/PPPP1PP1/RNBQKBNR b - - 0 1')
 # stockfish.set_fen_position('rnbqkb1r/ppppp1pp/8/2n5/4K3/8/8/R7 b - - 0 1')
-stockfish.set_fen_position('6q1/1P1p2p1/K5p1/8/1Pp2B1P/1b1P4/3nP3/k1b5 w - - 0 1')
-print(stockfish.get_board_visual())
-move = stockfish.get_best_move_time(200)
-print(move)
+# stockfish.set_fen_position('6q1/1P1p2p1/K5p1/8/1Pp2B1P/1b1P4/3nP3/k1b5 w - - 0 1')
+# print(stockfish.get_board_visual())
+# move = stockfish.get_best_move_time(200)
+# print(move)
 
 import pyttsx3
 engine = pyttsx3.init()
