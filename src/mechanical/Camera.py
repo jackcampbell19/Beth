6cfab682df0e86dfdca428fda9d4364f2b9d7312
Jack Campbell <jackcampbell19@gmail.com>
2021-08-22 19:55:11 -0700

    update

--- src/mechanical/Camera.py
+++ src/mechanical/Camera.py
@@ -75,6 +75,7 @@ class Camera:
             self.latest_frame = frame
             return frame
         camera = self.generate_camera()
+        camera.set(cv2.CAP_PROP_EXPOSURE, -1)
         log.info('Warming camera up.')
         ret, frame = camera.read()
         for i in range(20):
