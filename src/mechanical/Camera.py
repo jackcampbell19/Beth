5de7047ebbcebccdea590a33d9877be307b52ae3
Jack Campbell <jackcampbell19@gmail.com>
2021-08-24 16:15:42 -0700

    update

--- src/mechanical/Camera.py
+++ src/mechanical/Camera.py
@@ -43,13 +43,6 @@ class Camera:
         print(f"CAP_PROP_AUTO_EXPOSURE: {camera.get(cv2.CAP_PROP_AUTO_EXPOSURE)}")
         print(f"CAP_PROP_EXPOSURE: {camera.get(cv2.CAP_PROP_EXPOSURE)}")
         print(f"CAP_PROP_BRIGHTNESS: {camera.get(cv2.CAP_PROP_BRIGHTNESS)}")
-        camera.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
-        camera.set(cv2.CAP_PROP_EXPOSURE, 0.005)
-        print()
-        print(f"CAP_PROP_CONTRAST: {camera.get(cv2.CAP_PROP_CONTRAST)}")
-        print(f"CAP_PROP_AUTO_EXPOSURE: {camera.get(cv2.CAP_PROP_AUTO_EXPOSURE)}")
-        print(f"CAP_PROP_EXPOSURE: {camera.get(cv2.CAP_PROP_EXPOSURE)}")
-        print(f"CAP_PROP_BRIGHTNESS: {camera.get(cv2.CAP_PROP_BRIGHTNESS)}")
         return camera
 
     def correct_distortion(self, frame):
