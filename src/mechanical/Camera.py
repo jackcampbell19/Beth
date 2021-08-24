7a42e9c13f0707cb129024dc9d097c248282740b
Jack Campbell <jackcampbell19@gmail.com>
2021-08-24 16:58:24 -0700

    update

--- src/mechanical/Camera.py
+++ src/mechanical/Camera.py
@@ -39,7 +39,7 @@ class Camera:
         camera.set(cv2.CAP_PROP_BUFFERSIZE, 3)
         e = camera.get(cv2.CAP_PROP_EXPOSURE)
         camera.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
-        camera.set(cv2.CAP_PROP_EXPOSURE, 0.003)
+        camera.set(cv2.CAP_PROP_EXPOSURE, 0.002)
         print(f"CAP_PROP_EXPOSURE: {e} -> {camera.get(cv2.CAP_PROP_EXPOSURE)}")
         for _ in range(60):
             _, _ = camera.read()
