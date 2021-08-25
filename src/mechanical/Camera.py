51f579ffb3d82897ab630baff9b2ca2ac974bfcd
Jack Campbell <jackcampbell19@gmail.com>
2021-08-24 19:38:20 -0700

    update

--- src/mechanical/Camera.py
+++ src/mechanical/Camera.py
@@ -39,9 +39,11 @@ class Camera:
         camera.set(cv2.CAP_PROP_BUFFERSIZE, 3)
         e = camera.get(cv2.CAP_PROP_EXPOSURE)
         camera.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
-        camera.set(cv2.CAP_PROP_EXPOSURE, 0.002)
-        print(f"CAP_PROP_EXPOSURE: {e} -> {camera.get(cv2.CAP_PROP_EXPOSURE)}")
-        for _ in range(60):
+        # 0.002 and 0.003 are good for bright light, daytime for example.
+        daytime = 0.002
+        night = 0.005
+        camera.set(cv2.CAP_PROP_EXPOSURE, night)
+        for _ in range(25):
             _, _ = camera.read()
         return camera
 
