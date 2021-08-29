1c644e18d9afbd206b8c16695ae3c58d0ea6374c
Jack Campbell <jackcampbell19@gmail.com>
2021-08-29 15:41:36 -0700

    update

--- src/main.py
+++ src/main.py
@@ -382,7 +382,7 @@ if __name__ == "__main__":
             for x in [0.0005, 0.001, 0.002, 0.003, 0.004, 0.005, 0.006]:
                 f = camera.capture_frame(exposure=x)
                 save_frame_to_runtime_dir(f, camera, name=x)
-                m = Marker.extract_markers(f)
+                m = Marker.extract_markers(f, marker_family=Marker.FAMILY_tag16h5)
                 draw_markers(f, m)
                 save_frame_to_runtime_dir(f, camera, name=f"{x}-markers")
         else:
